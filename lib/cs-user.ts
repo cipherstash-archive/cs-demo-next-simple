b40a3f7013523894690492de3c0578da82751dde
Dan Draper <dan@coderdan.co>
2022-04-08 12:36:11 +1000

    Added lazy collection loading

--- lib/cs-user.ts
+++ lib/cs-user.ts
@@ -3,39 +3,41 @@ import { Stash } from "@cipherstash/stashjs"
 import { v5 as uuidv5 } from 'uuid'
 
 type CSUser = Omit<User, "id"> & { originalId: number, id: string }
+const ID_NAMESPACE = '1b671a64-40d5-491e-99b0-da01ff1f3341'
 
-export default (async function() {
-  const ID_NAMESPACE = '1b671a64-40d5-491e-99b0-da01ff1f3341'
-  const stash = await Stash.connect()
-  const users = await stash.loadCollection<CSUser>("users")
+const lazyUsers = Stash.connect()
+  .then(stash => stash.loadCollection<CSUser>("users"))
 
-  return {
-    listUsers: async function(): Promise<Array<User>> {
-      return await users.query({})
-        .then(users => users.documents.map(user => (
-          { ...user, id: user.originalId }
-        )))
-    },
+export default {
+  listUsers: async function(): Promise<Array<User>> {
+    const users = await lazyUsers
+    return await users.query({})
+    .then(users => users.documents.map(user => (
+      { ...user, id: user.originalId }
+    )))
+  },
 
-    putUser: async function(user: User): Promise<string> {
-      let mappedId = uuidv5(`${user.id}`, ID_NAMESPACE)
+  putUser: async function(user: User): Promise<string> {
+    let mappedId = uuidv5(`${user.id}`, ID_NAMESPACE)
+    const users = await lazyUsers
 
-      return await users.put({
-        ...user,
-        id: mappedId,
-        originalId: user.id
-      })
-    },
+    return await users.put({
+      ...user,
+      id: mappedId,
+      originalId: user.id
+    })
+  },
 
-    getUser: async function(id: number): Promise<User> {
-      let mappedId = uuidv5(`${id}`, ID_NAMESPACE)
-      let user = await users.get(mappedId)
+  getUser: async function(id: number): Promise<User> {
+    let mappedId = uuidv5(`${id}`, ID_NAMESPACE)
+    const users = await lazyUsers
+    let user = await users.get(mappedId)
 
-      if (user) {
-        return { ...user, id: user.originalId }
-      } else {
-        throw(`No user with id=${id}`)
-      }
+    if (user) {
+      return { ...user, id: user.originalId }
+    } else {
+      throw(`No user with id=${id}`)
     }
   }
-})()
+}
+
